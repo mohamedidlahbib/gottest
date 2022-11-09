14e6f8dfe2bd9cbd21a9c93d7842701a8982f4f5
med <muhaid35@gmail.com>
2022-11-09 17:14:19 +0100

    yassin

---
+++ dairy/src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+			Parent root =  loader.load();
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
